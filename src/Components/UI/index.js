a2204754503a337b2222d5ccc1e6fd4f5719df12
Anthony Maras <marasantonis@gmail.com>
2022-10-03 12:58:29 +0300

    index update

--- src/Components/UI/index.js
+++ src/Components/UI/index.js
@@ -2,12 +2,5 @@ import { ButtonPrimary } from './Buttons/ButtonPrimary';
 import { ButtonSecondary } from './Buttons/ButtonSecondary';
 import { MenuButtonBig } from './Buttons/MenuButtonBig';
 import { MenuSelection } from './Buttons/MenuSelection';
-import { GameTitle } from './GameTitle';
 
-export {
-  ButtonPrimary,
-  ButtonSecondary,
-  MenuButtonBig,
-  MenuSelection,
-  GameTitle,
-};
+export { ButtonPrimary, ButtonSecondary, MenuButtonBig, MenuSelection };
