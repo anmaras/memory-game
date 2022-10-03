741acab8d40550b6f09060c672488b88ea43aafe
Anthony Maras <marasantonis@gmail.com>
2022-10-03 12:56:38 +0300

    delete file

--- src/Components/UI/GameTitle.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import style from '../../styles/Components/UI/GameTitle.module.css';
-
-export const GameTitle = ({ classInput = 'game-title' }) => {
-  return <h1 className={style[`${classInput}`]}>memory</h1>;
-};
