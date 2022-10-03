f085624565cb515516eaf6c6e4aa7b6de0dfa77a
Anthony Maras <marasantonis@gmail.com>
2022-10-03 12:59:15 +0300

    add header, grid and footer component

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { ButtonPrimary, GameTitle } from './UI';
+import { GameHeader, GameGrid, GameFooter } from './GameSolo';
 import style from '../styles/Components/Game.module.css';
 
 export const Game = () => {
   return (
-    <section style={style.game}>
-      <div className={style['game--header']}>
-        <GameTitle />
-        <ButtonPrimary textInput="menu" />
-      </div>
-      <div style={style['game--grid']}></div>
-      <div className={style['game--info']}></div>
-    </section>
+    <article className={style.game}>
+      <GameHeader />
+      <GameGrid />
+      <GameFooter />
+    </article>
   );
 };
