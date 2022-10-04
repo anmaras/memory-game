bbc6075598dbc81a2c94b162632abf9d06b4334a
Anthony Maras <marasantonis@gmail.com>
2022-10-04 10:04:40 +0300

    add array classes

--- src/Components/GameSolo/GameGrid/GameGrid.js
+++ src/Components/GameSolo/GameGrid/GameGrid.js
@@ -5,10 +5,13 @@ import style from '../../../styles/Components/GameSolo/GameGrid.module.css';
 export const GameGrid = () => {
   return (
     <section className={style['grid-container']}>
-      <ul className={style['grid-list']}>
+      <ul className={[style['grid-list'], style.list16].join(' ')}>
         {[...Array(16)].map((item, index) => {
           return (
-            <li className={style['grid-list-item']} key={index}>
+            <li
+              className={[style['grid-list-item'], style.grid16].join(' ')}
+              key={index}
+            >
               <GameGridItem input={index} />
             </li>
           );
--- src/Components/GameSolo/GameGrid/GameGridItem.js
+++ src/Components/GameSolo/GameGrid/GameGridItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import style from '../../../styles/Components/GameSolo/GameGrid.module.css';
 
 export const GameGridItem = ({ input }) => {
-  return <p>{input}</p>;
+  return <p className={[style['grid-item']].join(' ')}>{input}</p>;
 };
