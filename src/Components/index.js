4bac0e64f89178d85b6efe4419f8dcefe164f0bc
Anthony Maras <marasantonis@gmail.com>
2022-10-04 11:19:06 +0300

    index export update

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,6 @@
 import { StartGame } from './StartGame';
 import { Game } from './Game';
+import GameEndModal from './GameModals/GameEndModal';
+import { GameMenuModal } from './GameModals/GameMenuModal';
 
-export { StartGame, Game };
+export { StartGame, Game, GameEndModal, GameMenuModal };
