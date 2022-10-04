d0b750fa790f56e5e77965e74532486047c8090d
Anthony Maras <marasantonis@gmail.com>
2022-10-04 11:17:21 +0300

    add prop for text

--- src/Components/UI/Buttons/ButtonSecondary.js
+++ src/Components/UI/Buttons/ButtonSecondary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from '../../../styles/Components/UI/Buttons.module.css';
 
-export const ButtonSecondary = () => {
-  return <button className={style.buttonSecondary}>new game</button>;
+export const ButtonSecondary = ({ textInput = 'Secondary' }) => {
+  return <button className={style.buttonSecondary}>{textInput}</button>;
 };
