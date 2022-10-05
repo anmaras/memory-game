78a141af824ebb24e03a129c02acd292cb6c3524
Anthony Maras <marasantonis@gmail.com>
2022-10-05 20:16:17 +0300

    add prop input

--- src/Components/GameSolo/GameHeader/GameHeader.js
+++ src/Components/GameSolo/GameHeader/GameHeader.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { ButtonPrimary } from '../../UI';
 import style from '../../../styles/Components/GameSolo/GameHeader.module.css';
 
-const GameHeader = () => {
+const GameHeader = ({ modal }) => {
   return (
     <header className={style.header}>
       <h1 className={style['game-title']}>memory</h1>
-      <ButtonPrimary textInput="menu" />
+      <ButtonPrimary textInput="menu" modal={modal} />
     </header>
   );
 };
