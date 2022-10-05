2b81a9855d6340a0c067bad3821b9e3807cf792c
Anthony Maras <marasantonis@gmail.com>
2022-10-05 20:18:29 +0300

    app state added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import style from './styles/App/App.module.css';
-
 import { Game, StartGame, GameModal } from './Components';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <main className={style.app}>
-      {/* <StartGame /> */}
-      <GameModal />
-      <Game />
-    </main>
-  );
+  const [theme, setTheme] = useState(null);
+  const [gridSize, setGridSize] = useState(null);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
+
+  const updateTheme = (input) => setTheme(input);
+  const selectGridSize = (input) => setGridSize(input);
+  const startGame = (input) => (theme && gridSize ? setGameStarted(input) : '');
+  const modalVisibility = (input) => setModalVisible(input);
+  const newGame = () => {
+    setTheme(null);
+    setGridSize(null);
+    setGameStarted(false);
+    setModalVisible(false);
+  };
+  const restartGame = () => {
+    setShuffle(!shuffle);
+  };
+
+  if (!gameStarted) {
+    return (
+      <main className={style.app}>
+        <StartGame
+          changeTheme={updateTheme}
+          changeGrid={selectGridSize}
+          gameStarted={startGame}
+        />
+      </main>
+    );
+  } else {
+    return (
+      <main className={style.app}>
+        <Game
+          theme={theme}
+          grid={gridSize}
+          modal={modalVisibility}
+          shuffle={shuffle}
+        />
+        {modalVisible && (
+          <GameModal
+            modal={modalVisibility}
+            newGame={newGame}
+            restartGame={restartGame}
+          />
+        )}
+      </main>
+    );
+  }
 }
 
 export default App;
