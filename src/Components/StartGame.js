2bb1821ab686e7cbe75ead5cd2455fa1f9e7d031
Anthony Maras <marasantonis@gmail.com>
2022-10-05 20:17:43 +0300

    start game props added and passed to children

--- src/Components/StartGame.js
+++ src/Components/StartGame.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import { MenuButtonBig, MenuSelection } from './UI';
 import style from '../styles/Components/StartGame.module.css';
 
-export const StartGame = () => {
+const StartGame = ({ changeTheme, changeGrid, gameStarted }) => {
   return (
     <article className={style['section--game-start']}>
       <div>
@@ -11,23 +10,25 @@ export const StartGame = () => {
       <div className={style.gameContainer}>
         <h3>select theme</h3>
         <div className={style['menu--selection-buttons']}>
-          <MenuSelection textInput="numbers" />
-          <MenuSelection textInput="icons" />
+          <MenuSelection textInput="numbers" onClick={changeTheme} />
+          <MenuSelection textInput="icons" onClick={changeTheme} />
         </div>
-        <h3>number of players</h3>
+        {/* <h3>number of players</h3>
         <div className={style['menu--players-numbers']}>
           <MenuSelection textInput="1" />
           <MenuSelection textInput="2" />
           <MenuSelection textInput="3" />
           <MenuSelection textInput="4" />
-        </div>
+        </div> */}
         <h3>grid size</h3>
         <div className={style['menu--grid-size']}>
-          <MenuSelection textInput="4x4" />
-          <MenuSelection textInput="6x6" />
+          <MenuSelection textInput="4x4" onClick={changeGrid} />
+          <MenuSelection textInput="6x6" onClick={changeGrid} />
         </div>
-        <MenuButtonBig />
+        <MenuButtonBig onClick={gameStarted} />
       </div>
     </article>
   );
 };
+
+export { StartGame };
