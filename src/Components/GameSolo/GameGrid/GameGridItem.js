52ddb2382cbae2400de4cbbba8bca1cd6324be5d
Anthony Maras <marasantonis@gmail.com>
2022-10-06 16:43:44 +0300

    deleted item

--- src/Components/GameSolo/GameGrid/GameGridItem.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import style from '../../../styles/Components/GameSolo/GameGrid.module.css';
-
-export const GameGridItem = ({ input }) => {
-  return <p className={[style['grid-item']].join(' ')}>{input}</p>;
-};
