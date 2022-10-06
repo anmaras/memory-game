4b66e0e36984761e6ae044896a9229e47f3a72c9
Anthony Maras <marasantonis@gmail.com>
2022-10-06 19:18:42 +0300

    func to pass player moves to footer

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import { GameHeader, GameGrid, GameFooter } from './GameSolo';
 import style from '../styles/Components/Game.module.css';
 
 export const Game = ({ theme, grid, modal, shuffle }) => {
+  const [moves, setMoves] = useState(0);
+  const getPlayerMoves = (moves) => {
+    return setMoves(moves);
+  };
+
   return (
     <article className={style.game}>
       <GameHeader modal={modal} />
-      <GameGrid theme={theme} grid={grid} shuffling={shuffle} />
-      <GameFooter />
+      <GameGrid
+        theme={theme}
+        grid={grid}
+        shuffling={shuffle}
+        getMoves={getPlayerMoves}
+      />
+      <GameFooter playerMoves={moves} />
     </article>
   );
 };
