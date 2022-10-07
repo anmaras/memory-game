65fb8e68bc90c89d292bfcefeb4cc64175e7f330
Anthony Maras <marasantonis@gmail.com>
2022-10-07 10:05:01 +0300

    clear unececcery functions

--- src/utils/helpers/iconArrayMaker.js
+++ src/utils/helpers/iconArrayMaker.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import {
   faCoffee,
   faShieldHalved,
@@ -52,30 +50,4 @@ const iconArr4x4 = [
   faGift,
 ];
 
-/* Fisher-Yates Shuffle  */
-function shuffle(c) {
-  let arr = '';
-  for (let i = c.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    arr = c[j];
-    c[j] = c[i];
-    c[i] = arr;
-  }
-  return c;
-}
-
-const iconArray = (size) => {
-  let newArray = [];
-  size === 6 ? (newArray = iconArr6x6) : (newArray = iconArr4x4);
-  return shuffle(newArray.concat(newArray)).map((item) => (
-    <FontAwesomeIcon icon={item} />
-  ));
-};
-
-const numberArray = (size) => {
-  let num = '';
-  size === 6 ? (num = 18) : (num = 8);
-  return shuffle([...Array(num).keys(), ...Array(num).keys()]);
-};
-
-export { iconArray, numberArray, iconArr6x6, iconArr4x4, shuffle };
+export { iconArr6x6, iconArr4x4 };
