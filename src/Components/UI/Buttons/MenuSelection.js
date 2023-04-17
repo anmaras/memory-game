55763de647e6d31ca65cf072c2a10624f0051d3a
anmaras <marasantonis@gmail.com>
2023-04-17 12:32:31 +0300

    delete comments

--- src/Components/UI/Buttons/MenuSelection.js
+++ src/Components/UI/Buttons/MenuSelection.js
@@ -1,18 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from '../../../styles/Components/UI/MenuSelection.module.css';
 
 export const MenuSelection = ({ textInput = 'default text', onClick }) => {
-  const input = useRef(null);
+  const [theme, setTheme] = useState();
+  const [grid, setGrid] = useState();
+
+  const test = (e) => {};
+
+  useEffect(() => {
+    console.log(theme);
+  }, [theme]);
+
   return (
     <button
-      className={style['menuSelection']}
-      ref={input}
-      onClick={() => {
-        if (input.current.textContent === '4x4') {
+      className={[
+        style['menuSelection'],
+        `${theme === 'icons' ? style.isActive : ''}`,
+      ].join(' ')}
+      onClick={(e) => {
+        if (e.target.textContent === '4x4') {
           onClick(4);
-        } else if (input.current.textContent === '6x6') {
+          setGrid(e.target.textContent);
+        }
+        if (e.target.textContent === '6x6') {
           onClick(6);
-        } else onClick(input.current.textContent);
+          setGrid(e.target.textContent);
+        }
+        if (e.target.textContent === 'numbers') {
+          onClick(e.target.textContent);
+          test(e.target.textContent);
+        }
+        if (e.target.textContent === 'icons') {
+          onClick(e.target.textContent);
+          test(e.target.textContent);
+        }
       }}
     >
       {textInput}
